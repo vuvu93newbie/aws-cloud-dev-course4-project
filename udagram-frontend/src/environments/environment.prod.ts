5d05076d64c0df36ab978bf8ad1c1d4f98976dbe
vuvu93newbie <tnmvu93@hotmail.com>
2024-12-10 13:31:01 +0700

    update env with public network

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'a63fdac5d0c3e4b979b029b176c85aa6-668803658.us-east-1.elb.amazonaws.com:8080/api/0'
 };
 
 /*
